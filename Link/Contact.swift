6951603d4f145b0938ba7ea62b66027fb56f6176
milot31 <milot.phil@gmail.com>
2017-01-03 22:42:34 -0500

    new cells

---
+++ Link/Contact.swift
@@ -0,0 +1,13 @@
+//
+//  Contact.swift
+//  Link
+//
+//  Created by Phil Milot on 1/3/17.
+//  Copyright © 2017 Phil Milot. All rights reserved.
+//
+
+import Foundation
+
+class Contact: NSObject {
+    
+}
--- Link/ContactTableViewCell.swift
+++ Link/ContactTableViewCell.swift
@@ -10,4 +10,18 @@ import Foundation
 
 class ContactTableViewCell : UITableViewCell {
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        nameLabel.text = ""
+        locationLabel.text = ""
+        dateLabel.text = ""
+    }
 }
